dafd827e6b0d377657b248a77172a5e96dfdb27e
Nasir Uddin <monforingnasir@gmail.com>
2021-02-12 19:07:41 +0600

    add typeScript

--- dist/first.js
+++ dist/first.js
@@ -2,3 +2,9 @@
 let myMoney = 50;
 myMoney = 150;
 myMoney = 0;
+const isFamous = true;
+console.log(isFamous);
+let famous = false;
+console.log(famous);
+let club = "Real Madrid";
+console.log(club);
