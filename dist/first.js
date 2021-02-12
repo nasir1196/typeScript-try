bbcb1aba2d6b1ea6447b2e99a1b981df747b06fb
Nasir Uddin <monforingnasir@gmail.com>
2021-02-12 20:26:44 +0600

    add typeScript

--- dist/first.js
+++ dist/first.js
@@ -8,3 +8,52 @@ let famous = false;
 console.log(famous);
 let club = "Real Madrid";
 console.log(club);
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(12, 15));
+console.log(add(4, "Name"));
+function fullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+const user = fullName("Ben", "Adam");
+let multiply;
+multiply = (x, y) => x * y;
+console.log(multiply(10, 12));
+let number = [2, 3, 4, 5, 6, 7, 8];
+number.push(9, 10);
+console.log(number);
+let friend = ["Nasir", "Tusher", "jubaer", "mazharul"];
+let megaFriend = "";
+for (let i = 0; i < friend.length; i++) {
+    const friendList = friend[i];
+    if (friendList.length > megaFriend.length) {
+        megaFriend = friendList;
+    }
+}
+console.log(megaFriend);
+// object
+let player = {
+    club: "Real Madrid",
+    salary: 454000,
+};
+console.log(player);
+const messy = {
+    name: "messy",
+    age: 40,
+    salary: 450000000,
+    club: "Barcelona",
+    wife: "some one",
+    isPlaying: true,
+};
+const rolando = {
+    name: "messy",
+    age: 30,
+    salary: 510000000,
+    club: "real madrid",
+    isPlaying: true,
+};
+function getBonus(player) {
+    return player.salary * 0.1;
+}
+getBonus(messy);
--- first.ts
+++ first.ts
@@ -8,3 +8,76 @@ let famous: boolean = false;
 console.log(famous);
 let club: string = "Real Madrid";
 console.log(club);
+
+function add(num1: any, num2: number | string): void {
+  return num1 + num2;
+}
+console.log(add(12, 15));
+console.log(add(4, "Name"));
+
+function fullName(firstName: string, lastName: string): string {
+  return firstName + " " + lastName;
+}
+const user: string = fullName("Ben", "Adam");
+
+let multiply: (x: number, y: number) => number;
+multiply = (x, y) => x * y;
+console.log(multiply(10, 12));
+
+let number: number[] = [2, 3, 4, 5, 6, 7, 8];
+number.push(9, 10);
+console.log(number);
+
+let friend: string[] = ["Nasir", "Tusher", "jubaer", "mazharul"];
+let megaFriend: string = "";
+for (let i = 0; i < friend.length; i++) {
+  const friendList: string = friend[i];
+  if (friendList.length > megaFriend.length) {
+    megaFriend = friendList;
+  }
+}
+console.log(megaFriend);
+
+// object
+
+let player: {
+  club: string;
+  salary: number;
+} = {
+  club: "Real Madrid",
+  salary: 454000,
+};
+console.log(player);
+
+// interface
+interface Player {
+  name: string;
+  age: number;
+  salary: number;
+  club: string;
+  wife?: string;
+  isPlaying: boolean;
+}
+
+const messy: Player = {
+  name: "messy",
+  age: 40,
+  salary: 450000000,
+  club: "Barcelona",
+  wife: "some one",
+  isPlaying: true,
+};
+
+const rolando: Player = {
+  name: "messy",
+  age: 30,
+  salary: 510000000,
+  club: "real madrid",
+  isPlaying: true,
+};
+
+function getBonus(player: Player) {
+  return player.salary * 0.1;
+}
+
+getBonus(messy);
